73e2c18c0349dbfaa48c5ad2c4efbbe240b8d458
dip222 <139950978+dip222@users.noreply.github.com>
2023-08-21 11:46:24 +0530

    first commit

---
+++ NewDev/src/components/Hero.jsx
@@ -0,0 +1,46 @@
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { ComputersCanvas } from "./canvas";
+
+const Hero = () => {
+  return (
+    <section className={`relative w-full h-screen mx-auto`}>
+      <div
+        className={`absolute inset-0 top-[140px] font-style: italic  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+      >
+        <div>
+          <h1 className={`${styles.heroHeadText} text-[#42dec9]`}>
+            Hi, I'm <span className='text-[#6eff5e]'>Dipan</span>
+          </h1>
+          <p className={`${styles.heroSubText} mt-1 text-[#dbdd4b] hover:text-gray-200`}>
+           I'm a skilled software developer <br className='sm:block hidden' />
+           UI/UX Designer
+          </p>
+        </div>
+      </div>
+
+      <ComputersCanvas />
+
+      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+        <a href='#about'>
+          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+            <motion.div
+              animate={{
+                y: [0, 24, 0],
+              }}
+              transition={{
+                duration: 0.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className='w-3 h-3 rounded-full bg-secondary mb-1'
+            />
+          </div>
+        </a>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ NewDev/src/constants/index.js
@@ -0,0 +1,231 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    starbucks,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    figma,
+    docker,
+    meta,
+    tesla,
+    shopify,
+    carrent,
+    jobit,
+    tripguide,
+    threejs,
+  } from "../assets";
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "Web Developer",
+      icon: web,
+    },
+    {
+      title: "React Native Developer",
+      icon: mobile,
+    },
+    {
+      title: "Backend Developer",
+      icon: backend,
+    },
+    {
+      title: "Python Developer",
+      icon: creator,
+    },
+    {
+      title: "Mobile Application Developer",
+      icon: web,
+    },
+    {
+      title: "Frontend Developer",
+      icon: web,
+    },
+  ];
+  
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "TypeScript",
+      icon: typescript,
+    },
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    {
+      name: "Redux Toolkit",
+      icon: redux,
+    },
+    {
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+    {
+      name: "Node JS",
+      icon: nodejs,
+    },
+    {
+      name: "MongoDB",
+      icon: mongodb,
+    },
+    {
+      name: "Three JS",
+      icon: threejs,
+    },
+    {
+      name: "git",
+      icon: git,
+    },
+    {
+      name: "figma",
+      icon: figma,
+    },
+    {
+      name: "docker",
+      icon: docker,
+    },
+  ];
+  
+  const experiences = [
+    {
+      title: "Secondary Education",
+      company_name: "86.4%",
+      icon: starbucks,
+      iconBg: "#383E56",
+      date: "2015-2021",
+      points: [
+       "Taranagar JogerdraNath High School"
+      ],
+    },
+    {
+      title: "Higher Secondary Education",
+      company_name: "",
+      icon: tesla,
+      iconBg: "#E6DEDD",
+      date: "2021-2023",
+      points: [
+        "Rajpur VidyaNidhi High School"
+      ],
+    },
+    {
+      title: "B.tech CSE with AI & ML",
+      company_name: "Shopify",
+      icon: shopify,
+      iconBg: "#383E56",
+      date: "2023-present",
+      points: [
+        "Bengal Institute of technology"
+      ],
+    },
+    
+  ];
+  
+  const testimonials = [
+    {
+      testimonial:
+        "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+      name: "Sara Lee",
+      designation: "CFO",
+      company: "Acme Co",
+      image: "https://randomuser.me/api/portraits/women/4.jpg",
+    },
+    {
+      testimonial:
+        "I've never met a web developer who truly cares about their clients' success like Rick does.",
+      name: "Chris Brown",
+      designation: "COO",
+      company: "DEF Corp",
+      image: "https://randomuser.me/api/portraits/men/5.jpg",
+    },
+    {
+      testimonial:
+        "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+      name: "Lisa Wang",
+      designation: "CTO",
+      company: "456 Enterprises",
+      image: "https://randomuser.me/api/portraits/women/6.jpg",
+    },
+  ];
+  
+  const projects = [
+    {
+      name: "Coming Soon...",
+      description:
+        "Coming Soon...",
+      tags: [
+        {
+          name: "Coming Soon...",
+          color: "blue-text-gradient",
+        },
+        
+      ],
+      image: "Coming Soon...",
+      source_code_link: "Coming Soon...",
+    },
+    {
+      name: "Coming Soon...",
+      description:
+        "Coming Soon...",
+      tags: [
+        {
+          name: "Coming Soon...",
+          color: "blue-text-gradient",
+        },
+       
+      ],
+      image: "Coming Soon...",
+      source_code_link: "Coming Soon...",
+    },
+    {
+      name: "Coming Soon...",
+      description:
+        "Coming Soon...",
+      tags: [
+        {
+          name: "Coming Soon...",
+          color: "blue-text-gradient",
+        },
+       
+      ],
+      image: "Coming Soon...",
+      source_code_link: "Coming Soon...",
+    },
+  ];
+  
+  export { services, technologies, experiences, testimonials, projects };
